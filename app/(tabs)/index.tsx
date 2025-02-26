d70dfbe21f524457ce94644cc04093b914302dd6
sophiaz03 <sophiazuleta83@gmail.com>
2025-02-25 20:00:59 -0500

    nav changes

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+const Page = () =>{
+    return (
+        <View>
+            <Text>page</Text>
+        </View>
+    )
+}
+export default Page
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }}/>
+
+    </Stack>
+
+  )
+
 }
+
+export default Layout;
